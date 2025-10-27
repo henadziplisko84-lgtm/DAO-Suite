843ba21804c2b2bb7d254b85a60108989cbb2182
henadziplisko84-lgtm <henadziplisko84@gmail.com>
2025-10-27 20:16:10 +0100

    Create GovernanceToken.sol

---
+++ contracts/GovernanceToken.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract GovernanceToken is ERC20, ERC20Permit, ERC20Votes, Ownable {
+constructor() ERC20("Game Governance Token", "GGT") ERC20Permit("Game Governance Token") {
+_mint(msg.sender, 1_000_000 * 10 ** decimals()); // initial supply to deployer
+}
+
+
+// The following functions are overrides required by Solidity.
+function _afterTokenTransfer(address from, address to, uint256 amount)
+internal
+override(ERC20, ERC20Votes)
+{
+super._afterTokenTransfer(from, to, amount);
+}
+
+
+function _mint(address to, uint256 amount)
+internal
+override(ERC20, ERC20Votes)
+{
+super._mint(to, amount);
+}
+
+
+function _burn(address account, uint256 amount)
+internal
+override(ERC20, ERC20Votes)
+{
+super._burn(account, amount);
+}
+}
